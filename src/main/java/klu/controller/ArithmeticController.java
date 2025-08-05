dfbb20d4019cdb16a1c47d5c72611f8657a03f15
drkpn <drkpn2019@gmail.com>
2025-08-05 23:47:28 +0530

    V2

--- src/main/java/klu/controller/ArithmeticController.java
+++ src/main/java/klu/controller/ArithmeticController.java
@@ -13,12 +13,12 @@ public class ArithmeticController {
 	@GetMapping("/add/{A}/{B}")
 	public String add(@PathVariable("A") int a,@PathVariable("B") int b)
 	{
-		return "Addition = " + (a+b);
+		return "" + (a+b);
 	}
 	
 	@GetMapping("/sub/{A}/{B}")
 	public String sub(@PathVariable("A") int a, @PathVariable("B") int b)
 	{
-		return "Subtraction = " + (a-b);
+		return "" + (a-b);
 	}
 }
